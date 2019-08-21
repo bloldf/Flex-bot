b36656e079e07250c536693106b38f5fa2e7dce0
bloldf <48572660+bloldf@users.noreply.github.com>
2019-08-21 17:30:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2221,4 +2221,4 @@ if (message.content.startsWith(prefix + 'نقاطي')) {
 /////////////////////////////////////////////////////////////////////////ALPHA / ALPHA CODES / ALPHA CODES///////////////////////////
 
 
-client.login(NjEzNDA5MzkwMDQ4OTY4ODgw.XV1iMQ.n2tfu7upHGkPGGJcVqboGicsd_U);
+client.login(NjEzNDA5MzkwMDQ4OTY4ODgw.XV1jeg.PaE_vmjwU6bkBkbTgl7x7nycgmY);
